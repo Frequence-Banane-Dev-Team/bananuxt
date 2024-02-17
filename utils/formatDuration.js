1690096a149ae7a461e061b14b5ed55596576b7c
Antonin <antonin.faure@epfl.ch>
2024-02-18 00:38:59 +0100

    duration seconds display

--- utils/formatDuration.js
+++ utils/formatDuration.js
@@ -3,9 +3,13 @@ export const formatDuration = (duration) => {
 
     const hours = Math.floor(duration / 3600);
     const minutes = Math.floor((duration - hours * 3600) / 60);
+    const seconds = duration - hours * 3600 - minutes * 60;
     
 
     if (hours === 0) {
+        if (minutes === 0) {
+            return `${seconds}s`;
+        }
         return `${minutes}min`;
     }
 
