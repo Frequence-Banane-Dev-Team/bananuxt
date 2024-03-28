08ad0cc093a8e4bf62cb3bee68d41a59588041ff
antoninfaure <antonin.faure@epfl.ch>
2024-03-28 18:39:50 +0100

    parse rel url that should be abs

--- utils/parseMarkdown.js
+++ utils/parseMarkdown.js
@@ -4,5 +4,14 @@ import markdownParser from '@nuxt/content/transformers/markdown'
 // first arg to parse() is for id, which is unused
 export const parseMarkdown = md => {
     const modifiedMd = md.replace(/\\n/g, '\n\n');
+
+    // Ensure all links are treated as absolute URLs
+    modifiedMd = modifiedMd.replace(/\[([^\]]+)\]\((?!http)([^\)]+)\)/g, (match, text, url) => {
+        // Prefix the URL with "https://" if it doesn't start with "http"
+        // This handles http and https URLs correctly, assuming non-http URLs are intended to be absolute
+        const prefixedUrl = url.startsWith('http') ? url : `https://${url}`;
+        return `[${text}](${prefixedUrl})`;
+    });
+    
     return markdownParser.parse('custom.md', modifiedMd)
 }
